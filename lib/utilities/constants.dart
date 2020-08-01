65a7f8253a201f23e5c36bf8cff5b06d2a2d9094
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-08-01 19:56:12 +0530

    first commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kColorYellow = Color(0xffffc700);
+const Color kColorRed = Color(0xffff403b);
+const Color kColorBlack = Color(0xff000000);
+const Color kColorGrey = Color(0xff8d8d8d);
